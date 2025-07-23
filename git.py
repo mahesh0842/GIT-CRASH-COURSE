8788736031445ddb6a0ca974aa3ea360d3bf6307
Mahesh Yadav <mahesh767000@gmail.com>
2025-07-23 16:09:35 +0530

    feature b added

--- git.py
+++ git.py
@@ -18,3 +18,10 @@ obj1=mk()
 res=obj1.add()
 print(res)
 print("here is how we can create class")
+class mk2:
+        def add(self):
+                print("class created method printed")
+
+obj2=mk2()
+res=obj2.add()
+print(res)
